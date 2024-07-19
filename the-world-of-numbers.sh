0f2194367591c1b758fe0c855fc3306b47b4043c
inura-perera <inuraperera6@gmail.com>
2024-07-19 11:56:49 +0530

    numbers challenge answers

---
+++ the-world-of-numbers.sh
@@ -0,0 +1,14 @@
+# Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+read x
+read y
+
+let "sum = x + y"
+let "difference = x - y"
+let "product = x*y"
+let "quotient = x/y"
+
+echo "$sum"
+echo "$difference"
+echo "$product"
+echo "$quotient"
